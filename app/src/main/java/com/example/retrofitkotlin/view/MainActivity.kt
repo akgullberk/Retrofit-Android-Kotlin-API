68ea2847d441bc16b64322686e3ecba5d3a1a3cf
Berk Akgül <berkak_gul51@hotmail.com>
2024-10-20 16:58:56 +0300

    model

--- app/src/main/java/com/example/retrofitkotlin/view/MainActivity.kt
+++ app/src/main/java/com/example/retrofitkotlin/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.retrofitkotlin
+package com.example.retrofitkotlin.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.retrofitkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
